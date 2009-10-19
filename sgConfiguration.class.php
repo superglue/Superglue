2140ba8858021e02b36e4b7ce1ae3331f91617a7
jscheel <jared@jaredscheel.com>
2009-10-19 18:01:38 +0000

    fixed twig include

--- sgConfiguration.class.php
+++ sgConfiguration.class.php
@@ -1,6 +1,6 @@
 <?php
 require(dirname(__FILE__) . '/sgContext.class.php');
-require(dirname(__FILE__) . '/vendor/Twig/Autoloader.php');
+require(dirname(__FILE__) . '/vendor/Twig/lib/Twig/Autoloader.php');
 
 class sgConfiguration
 {
