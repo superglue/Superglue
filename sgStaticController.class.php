b82ad56ac4db448a1fbb9378ac331359b351aae0
jscheel <jared@jaredscheel.com>
2009-10-20 17:13:31 +0000

    static controller now tries to guess title

--- sgStaticController.class.php
+++ sgStaticController.class.php
@@ -1,6 +1,8 @@
 <?php
 class sgStaticController extends sgBaseController {
   public function GET() {
+    $paths = explode('/', sgContext::getCurrentPath());
+    $this->title = ucwords(end($paths));
     return $this->render(substr(sgContext::getCurrentPath(), 1));
   }
 }
